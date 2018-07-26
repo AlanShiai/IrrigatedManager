dae0c9b5c600908a7414245be69df611dae948c1
AShi <apposa@126.com>
2018-07-26 19:04:37 +0800

    delete old project Inspect page Level2_2_projectInspect view.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_2_inspectNote.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_2_inspectNote.java
@@ -53,8 +53,6 @@ public class Level2_2_2_inspectNote extends AppCompatActivity {
         leve1_2_2_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Level2_2_2_inspectNote.this, Level2_2_projectInspection.class);
-                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetails.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetails.java
@@ -31,9 +31,6 @@ public class Level2_2_3_inspectDetails extends AppCompatActivity {
         findViewById(R.id.leve1_2_2_3_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Level2_2_3_inspectDetails.this, Level2_2_projectInspection.class);
-                startActivity(intent);
-                finish();
             }
         });
     }
