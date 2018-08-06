699266b6cba4ec989b0264e6f179cd35d9927e9b
AShi <apposa@126.com>
2018-08-07 07:43:13 +0800

    add more pic for overview page. - imageSwitch

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_1_irrigateOverview.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_1_irrigateOverview.java
@@ -1,23 +1,74 @@
 package com.example.ashi.irrigatedmanager;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.example.ashi.irrigatedmanager.util.Const;
 
-public class Level2_1_irrigateOverview extends AppCompatActivity {
+public class Level2_1_irrigateOverview extends AppCompatActivity implements ViewSwitcher.ViewFactory, View.OnTouchListener {
+
+    private ImageSwitcher mImageSwitcher;
+
+    private int[] imgIds;
+
+    private int currentPosition;
+
+    private  float downX;
+
+    private LinearLayout linearLayout;
+
+    private ImageView[] tips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level2_1_irrigate_overview);
+
+        imgIds = new int[] {
+                R.drawable.bing1,
+                R.drawable.bing2,
+                R.drawable.bing3,
+                R.drawable.bing4,
+                R.drawable.bing5,
+                R.drawable.bing6,
+                R.drawable.bing7,
+                R.drawable.bing8,
+                R.drawable.bing9,
+                R.drawable.bing10,
+        };
+
+        mImageSwitcher = (ImageSwitcher) findViewById(R.id.image_switcher);
+        mImageSwitcher.setFactory(this);
+        mImageSwitcher.setOnTouchListener(this);
+
+        linearLayout = (LinearLayout) findViewById(R.id.viewGroup);
+
+        tips = new ImageView[imgIds.length];
+        for (int i = 0; i < imgIds.length; i++) {
+            ImageView mImageView = new ImageView(this);
+            tips[i] = mImageView;
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            layoutParams.rightMargin = 3;
+            layoutParams.leftMargin = 3;
+
+            mImageView.setBackgroundResource(R.drawable.page_indicator_unfocused);
+            linearLayout.addView(mImageView, layoutParams);
+        }
+
+        mImageSwitcher.setImageResource(imgIds[currentPosition]);
+        setImageBackground(currentPosition);
 
         TextView content = (TextView) findViewById(R.id.level_2_1_content);
         content.setText(Const.LEVEL_2_CONTENT);
@@ -29,6 +80,16 @@ public class Level2_1_irrigateOverview extends AppCompatActivity {
             public void onClick(View v) {
             }
         });
+    }
+
+    private void setImageBackground(int selectItems) {
+        for (int i = 0; i < tips.length; i++ ) {
+            if ( i == selectItems ) {
+                tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
+            } else {
+                tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
+            }
+        }
     }
 
     private void addListernerForBottomToolbar() {
@@ -56,5 +117,56 @@ public class Level2_1_irrigateOverview extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                downX = event.getX();
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                float lastX = event.getX();
+                //抬起的时候的X坐标大于按下的时候就显示上一张图片
+                if(lastX > downX){
+                    if(currentPosition > 0){
+                        //设置动画，这里的动画比较简单，不明白的去网上看看相关内容
+//                        mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.left_in));
+//                        mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.right_out));
+                        currentPosition --;
+                        mImageSwitcher.setImageResource(imgIds[currentPosition % imgIds.length]);
+                        setImageBackground(currentPosition);
+                    }else{
+                        Toast.makeText(getApplication(), "已经是第一张", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                if(lastX < downX){
+                    if(currentPosition < imgIds.length - 1){
+//                        mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.right_in));
+//                        mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.lift_out));
+                        currentPosition ++ ;
+                        mImageSwitcher.setImageResource(imgIds[currentPosition]);
+                        setImageBackground(currentPosition);
+                    }else{
+                        Toast.makeText(getApplication(), "到了最后一张", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            break;
+        }
+        return true;
+    }
+
+    @Override
+    public View makeView() {
+        final ImageView i = new ImageView(this);
+        i.setBackgroundColor(0xFF000000);
+        i.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        i.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+        return i;
     }
 }
