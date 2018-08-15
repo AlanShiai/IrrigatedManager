64e69073fd662126c01628ff851dfde796d66b9d
AShi <apposa@126.com>
2018-08-15 11:16:48 +0800

    [QQ] use 2 pix as separator. Approval page use tab view.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_5_appvalProcess.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_5_appvalProcess.java
@@ -3,10 +3,17 @@ package com.example.ashi.irrigatedmanager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,6 +24,8 @@ import android.widget.Toast;
 import com.example.ashi.irrigatedmanager.gson.User;
 import com.example.ashi.irrigatedmanager.level2_2_3.InspectDetailInfo;
 import com.example.ashi.irrigatedmanager.level2_2_3.InspectDetailInfoAdpter;
+import com.example.ashi.irrigatedmanager.level2_5.ManualInspectBasicInfo;
+import com.example.ashi.irrigatedmanager.level2_5.ManualInspectBasicInfoAdapter;
 import com.example.ashi.irrigatedmanager.level5.Appval;
 import com.example.ashi.irrigatedmanager.level5.AppvalAdapter;
 import com.example.ashi.irrigatedmanager.util.Const;
@@ -33,56 +42,27 @@ import okhttp3.Response;
 
 public class Level2_5_appvalProcess extends AppCompatActivity {
 
-    private List<Appval> dataList = new ArrayList<Appval>();
+    private static List<Appval> dataList = new ArrayList<Appval>();
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level2_5_appval_process);
 
-        Button button = (Button) findViewById(R.id.should_do_task_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "http://www.boze-tech.com/zfh_manager/a/app/login/loginCheck?loginName=admin&passwd=123";
-                HttpUtil.sendOkHttpRequest(url, new Callback() {
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        final String responseText = response.body().string();
-                        Log.d("aijun login", responseText);
-                        final User user = Utility.handleLoginResponse(responseText);
-                        Log.d("aijun login", Boolean.toString(user.isLoginSuccess()));
-//                        final Weather weather = Utility.handleWeatherResponse(responseText);
-                    }
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        e.printStackTrace();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(Level2_5_appvalProcess.this, "登录失败", Toast.LENGTH_SHORT).show();
-//                                swipeRefresh.setRefreshing(false);
-                            }
-                        });
-                    }
-                });
-            }
-        });
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         initData();
-        AppvalAdapter adapter = new AppvalAdapter(
-                Level2_5_appvalProcess.this, R.layout.appval_item, dataList);
-        ListView listView = (ListView) findViewById(R.id.level_2_5_appval_list);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(Level2_5_appvalProcess.this, Level2_5_1_appvalDetails.class);
-                startActivity(intent);
-            }
-        });
 
         findViewById(R.id.leve1_2_5_back).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,5 +83,70 @@ public class Level2_5_appvalProcess extends AppCompatActivity {
         dataList.add(new Appval("巡查异常处理"));
         dataList.add(new Appval("巡查异常处理"));
         dataList.add(new Appval("巡查异常处理"));
+    }
+
+    public static class PlaceholderFragment1 extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_listview, container, false);
+
+            AppvalAdapter adapter = new AppvalAdapter(
+                    getContext(), R.layout.appval_item, dataList);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.fragment_listview_list);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(getContext(), Level2_5_1_appvalDetails.class);
+                    startActivity(intent);
+                }
+            });
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new PlaceholderFragment1();
+                case 1:
+                    return new PlaceholderFragment1();
+                case 2:
+                    return new PlaceholderFragment1();
+            }
+            return new PlaceholderFragment1();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "待办任务";
+                case 1:
+                    return "已办任务";
+                case 2:
+                    return "我发起的";
+            }
+            return null;
+        }
     }
 }
