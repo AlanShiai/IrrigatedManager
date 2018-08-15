4b075c95ce04d75101fbb08b1853bc14caad978e
AShi <apposa@126.com>
2018-08-15 15:03:14 +0800

    Level2_2_3_inspectDetailsPie: add select dialog.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetailsPie.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetailsPie.java
@@ -1,10 +1,13 @@
 package com.example.ashi.irrigatedmanager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.ashi.irrigatedmanager.level2_2_3.DrawColumnAndPie;
 import com.example.ashi.irrigatedmanager.level2_2_3.DrawYearMonthData;
@@ -28,6 +31,43 @@ public class Level2_2_3_inspectDetailsPie extends AppCompatActivity {
                 finish();
             }
         });
+        findViewById(R.id.select).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSingleChoiceDialog();
+            }
+        });
+    }
 
+    private void showSingleChoiceDialog() {
+        final String[] items = new String[]{"一月", "二月", "三月", "四月","五月", "六月", "七月", "八月","九月", "十月", "十一月", "十二月",};
+        AlertDialog.Builder builder = new AlertDialog.Builder(Level2_2_3_inspectDetailsPie.this);
+        builder.setTitle("选择对话框");
+        //千万不要加这句，不然列表显示不出来
+//        builder.setMessage("这是一个简单的列表对话框");
+        builder.setIcon(R.mipmap.launcher);
+        builder.setSingleChoiceItems(items, 1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                showText(items[which]);
+            }
+        });
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                showText("确定");
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                showText("取消");
+            }
+        });
+
+        builder.create().show();
+    }
+
+    private void showText(String text) {
+        Toast.makeText(Level2_2_3_inspectDetailsPie.this, text, Toast.LENGTH_SHORT).show();
     }
 }
