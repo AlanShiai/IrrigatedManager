8dda679461e2b4e1a24418290d1c8b1780f014fc
AShi <apposa@126.com>
2018-08-15 16:57:04 +0800

    scan and can get info

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_projectInspection2.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_projectInspection2.java
@@ -1,14 +1,23 @@
 package com.example.ashi.irrigatedmanager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.ashi.irrigatedmanager.level2_2_3.DrawYearMonthData;
+import com.acker.simplezxing.activity.CaptureActivity;
 
 public class Level2_2_projectInspection2 extends AppCompatActivity {
+
+    private static final int REQ_CODE_PERMISSION = 0x1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,25 @@ public class Level2_2_projectInspection2 extends AppCompatActivity {
         addListernerForBackButton();
     }
 
+    private void showText(String text) {
+        Toast.makeText(Level2_2_projectInspection2.this, text, Toast.LENGTH_SHORT).show();
+    }
+
     private void addListernerForBottomToolbar() {
+        findViewById(R.id.scan_inspect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Scan Activity
+                if (ContextCompat.checkSelfPermission(Level2_2_projectInspection2.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // Do not have the permission of camera, request it.
+                    ActivityCompat.requestPermissions(Level2_2_projectInspection2.this, new String[]{Manifest.permission.CAMERA}, REQ_CODE_PERMISSION);
+                } else {
+                    // Have gotten the permission
+                    startCaptureActivityForResult();
+                }
+
+            }
+        });
         findViewById(R.id.start_inspect).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,6 +84,58 @@ public class Level2_2_projectInspection2 extends AppCompatActivity {
             }
         });
     }
+
+    private void startCaptureActivityForResult() {
+        Intent intent = new Intent(Level2_2_projectInspection2.this, CaptureActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(CaptureActivity.KEY_NEED_BEEP, CaptureActivity.VALUE_BEEP);
+        bundle.putBoolean(CaptureActivity.KEY_NEED_VIBRATION, CaptureActivity.VALUE_VIBRATION);
+        bundle.putBoolean(CaptureActivity.KEY_NEED_EXPOSURE, CaptureActivity.VALUE_NO_EXPOSURE);
+        bundle.putByte(CaptureActivity.KEY_FLASHLIGHT_MODE, CaptureActivity.VALUE_FLASHLIGHT_OFF);
+        bundle.putByte(CaptureActivity.KEY_ORIENTATION_MODE, CaptureActivity.VALUE_ORIENTATION_AUTO);
+        bundle.putBoolean(CaptureActivity.KEY_SCAN_AREA_FULL_SCREEN, CaptureActivity.VALUE_SCAN_AREA_FULL_SCREEN);
+        bundle.putBoolean(CaptureActivity.KEY_NEED_SCAN_HINT_TEXT, CaptureActivity.VALUE_SCAN_HINT_TEXT);
+        intent.putExtra(CaptureActivity.EXTRA_SETTING_BUNDLE, bundle);
+        startActivityForResult(intent, CaptureActivity.REQ_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_CODE_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // User agree the permission
+                    startCaptureActivityForResult();
+                } else {
+                    // User disagree the permission
+                    Toast.makeText(this, "You must agree the camera permission request before you use the code scan function", Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CaptureActivity.REQ_CODE:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        showText(data.getStringExtra(CaptureActivity.EXTRA_SCAN_RESULT));
+                        break;
+                    case RESULT_CANCELED:
+                        if (data != null) {
+                            // for some reason camera is not working correctly
+                            showText(data.getStringExtra(CaptureActivity.EXTRA_SCAN_RESULT));
+                        }
+                        break;
+                }
+                break;
+        }
+    }
+
 
     private void addListernerForBackButton() {
         findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
