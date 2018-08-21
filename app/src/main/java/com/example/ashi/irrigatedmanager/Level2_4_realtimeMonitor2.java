7e9f2b546de074d07236f9934b865b00da383323
AShi <apposa@126.com>
2018-08-21 15:25:42 +0800

    config realtime monitor full screen

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_4_realtimeMonitor2.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_4_realtimeMonitor2.java
@@ -2,6 +2,8 @@ package com.example.ashi.irrigatedmanager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +24,12 @@ public class Level2_4_realtimeMonitor2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_level2_4_realtime_monitor2);
 
         initProjectInfoList();
@@ -91,22 +99,22 @@ public class Level2_4_realtimeMonitor2 extends AppCompatActivity {
     }
 
     private void addListernerForBottomToolbar() {
-        findViewById(R.id.rain).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Level2_4_realtimeMonitor2.this, Level2_4_3_rain2.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        findViewById(R.id.irrigation_schedule).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Level2_4_realtimeMonitor2.this, Level2_6_irrigationSchedule2.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        findViewById(R.id.rain).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Level2_4_realtimeMonitor2.this, Level2_4_3_rain2.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+//        findViewById(R.id.irrigation_schedule).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Level2_4_realtimeMonitor2.this, Level2_6_irrigationSchedule2.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 
     private void addListernerForBackButton() {
