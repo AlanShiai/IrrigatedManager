e7a32fcb9dd2891a9dad5e5d15830cfe50da4f11
AShi <apposa@126.com>
2018-08-22 19:03:21 +0800

    update 2_2_5_1 manual inspect to full screen

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_5_1_manualInspect.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_5_1_manualInspect.java
@@ -1,6 +1,8 @@
 package com.example.ashi.irrigatedmanager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +24,12 @@ public class Level2_2_5_1_manualInspect extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_level2_2_5_1_manual_inspect);
 
         initData();
