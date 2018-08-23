b8e995fbf9315c336ab095dd14641bcd73fcf32a
AShi <apposa@126.com>
2018-08-23 19:06:08 +0800

    update manual inspect page 3: set it full screen.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_5_3_manualInspect.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_5_3_manualInspect.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.DocumentsContract;
@@ -66,6 +67,13 @@ public class Level2_2_5_3_manualInspect extends AppCompatActivity {
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
         SDKInitializer.initialize(getApplicationContext());
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         setContentView(R.layout.activity_level2_2_5_3_manual_inspect);
 
