ae68c55d57e4de194b995e3473bcd7fb36e9c286
AShi <apposa@126.com>
2018-08-24 21:01:15 +0800

    level2_2_3_inspect_details_pie: set it full screen.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetailsPie.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetailsPie.java
@@ -2,6 +2,8 @@ package com.example.ashi.irrigatedmanager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,12 @@ public class Level2_2_3_inspectDetailsPie extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_level2_2_3_inspect_details_pie);
 
         LinearLayout ll_body = (LinearLayout) findViewById(R.id.inspect_detail_pie);
