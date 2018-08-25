47d851a60a1a6376a64daad440f0fafded930f48
AShi <apposa@126.com>
2018-08-25 15:19:41 +0800

    inspect_details: set it full screen.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetails2.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_3_inspectDetails2.java
@@ -2,6 +2,8 @@ package com.example.ashi.irrigatedmanager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +24,12 @@ public class Level2_2_3_inspectDetails2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_level2_2_3_inspect_details2);
 
         initData();
