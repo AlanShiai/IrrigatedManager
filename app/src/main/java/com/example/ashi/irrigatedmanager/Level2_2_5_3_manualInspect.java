046f8b1f014894a1206a12a1cf7ecc228fe626ad
AShi <apposa@126.com>
2018-08-31 22:50:48 +0800

    patrol save : longitude latitude.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_5_3_manualInspect.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_2_5_3_manualInspect.java
@@ -78,6 +78,9 @@ public class Level2_2_5_3_manualInspect extends AppCompatActivity {
 
     private TextView positionText;
 
+    private double latitude = 116.429489;
+    private double longitude = 39.87182;
+
 //    private MapView mapView;
 
 //    private BaiduMap baiduMap;
@@ -241,6 +244,7 @@ public class Level2_2_5_3_manualInspect extends AppCompatActivity {
                 // &goalId=8502f69d32304ee6a9aacd99920fdcd7%22%20+%20%22&longitude=116.429489&latitude=39.87182
                 // &images=&itemResults=&createBy=1&contents=%E6%98%AF%E6%98%AF%E6%98%AF&userId=1
                 String url = Api.API_22_patrolSave + "&type=" + Global.patrolType
+                        + "&longitude=" + longitude + "&latitude=" + latitude
                         + "&goalId=" + Global.patrolId + "&contents=" + editText.getText().toString()
                         + "&itemResults=" + Global.exceptionMsg;
                 Log.d("aijun, patrolSave", url);
@@ -283,6 +287,7 @@ public class Level2_2_5_3_manualInspect extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     String url = Api.API_22_patrolSave + "&type=" + Global.patrolType
+                            + "&longitude=" + longitude + "&latitude=" + latitude
                             + "&goalId=" + Global.patrolId + "&contents=" + editText.getText().toString()
                             + "&itemResults=";
                     Log.d("aijun, patrolSave", url);
@@ -507,6 +512,8 @@ public class Level2_2_5_3_manualInspect extends AppCompatActivity {
 //                    }
                     positionText.setText(currentPosition);
                     showText(currentPosition.toString());
+                    latitude = bdLocation.getLatitude();
+                    longitude = bdLocation.getLongitude();
                     mLocationClient.stop();
                 }
             });
