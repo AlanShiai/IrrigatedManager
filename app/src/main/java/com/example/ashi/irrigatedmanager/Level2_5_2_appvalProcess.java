9135b7e2ed97f5e82ed42898159530361a96e6e8
AShi <apposa@126.com>
2018-09-26 08:39:38 +0800

    handleProcess add dialog to provide dealtype selector.

--- app/src/main/java/com/example/ashi/irrigatedmanager/Level2_5_2_appvalProcess.java
+++ app/src/main/java/com/example/ashi/irrigatedmanager/Level2_5_2_appvalProcess.java
@@ -1,5 +1,6 @@
 package com.example.ashi.irrigatedmanager;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -11,17 +12,20 @@ import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.ashi.irrigatedmanager.gson.PatrolManager;
 import com.example.ashi.irrigatedmanager.gson.PatrolManagerAdpter;
 import com.example.ashi.irrigatedmanager.level5.AppvalAdapter;
 import com.example.ashi.irrigatedmanager.util.Api;
+import com.example.ashi.irrigatedmanager.util.DialogSelectItemAdapter;
 import com.example.ashi.irrigatedmanager.util.Global;
 import com.example.ashi.irrigatedmanager.util.HttpUtil;
 import com.example.ashi.irrigatedmanager.util.Utility;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import okhttp3.Call;
@@ -38,6 +42,10 @@ public class Level2_5_2_appvalProcess extends AppCompatActivity {
 
     EditText editText;
 
+    TextView dealtype_text;
+    List<String> items = Arrays.asList("其他科室","本科室");
+    int oldSelector = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +53,8 @@ public class Level2_5_2_appvalProcess extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.list_view);
         editText = (EditText) findViewById(R.id.editText);
+        dealtype_text = (TextView) findViewById(R.id.dealtype_text);
+        dealtype_text.setText(items.get(oldSelector));
 
         findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +77,36 @@ public class Level2_5_2_appvalProcess extends AppCompatActivity {
             public void onClick(View v) {
                 flag = "no";
                 doProcess();
+            }
+        });
+        findViewById(R.id.select_dealtype).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog builder = new Dialog(Level2_5_2_appvalProcess.this, R.style.update_dialog);
+                View view = View.inflate(Level2_5_2_appvalProcess.this, R.layout.dialog_select, null);
+                view.findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        builder.dismiss();
+                    }
+                });
+                final ListView dialogListView = (ListView) view.findViewById(R.id.list_view);
+                final DialogSelectItemAdapter adapter = new DialogSelectItemAdapter(
+                        Level2_5_2_appvalProcess.this, R.layout.dialog_select_item, items, oldSelector);
+                dialogListView.setAdapter(adapter);
+                dialogListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if (oldSelector != position) {
+                            oldSelector = position;
+                            dealtype_text.setText(items.get(oldSelector));
+                        }
+                        builder.dismiss();
+                    }
+                });
+                builder.setContentView(view);//这里还可以指定布局参数
+                builder.show();
+
             }
         });
 
@@ -102,7 +142,7 @@ public class Level2_5_2_appvalProcess extends AppCompatActivity {
         // &procDefKey=&businessKey=pro_patrol_result_deal:98eaf7ee37354b48b875caf30bbad7a9&userId=xizha&flag=yes
         Log.d("aijun, agreeProcess", Global.appval + "");
         String url = Api.API_15_handleProcess + "taskName=" + Utility.toURLEncoded(Global.appval.taskName)
-                + "&dealType=" + "1" + "&comment=" + Utility.toURLEncoded(editText.getText().toString()) + "&taskId=" + Global.appval.taskId
+                + "&dealType=" + oldSelector + "&comment=" + Utility.toURLEncoded(editText.getText().toString()) + "&taskId=" + Global.appval.taskId
                 + "&taskDefKey=" + Global.appval.taskDefKey + "&procInsId=" + Global.appval.processInstanceId
                 + "&procDefId=" + Global.appval.procDefId + "&procDefKey=" + ""
                 + "&businessKey=" + Global.appval.businessKey + "&userId=" + getPatrolManagerUserId()
